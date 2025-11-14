8696d3d7947afaab8622314fbf15b54b37fcb698
bengris32 <bengris32@protonmail.ch>
2025-11-14 17:41:11 +0000

    dt-bindings: clock: Add MT6757 peripheral clock and reset bindings
    Add clock and reset bindings for the camsys, imgsys, mfgcfg, vdecsys and
    vencsys blocks on the MT6757 SoC.
    
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

---
+++ include/dt-bindings/clock/mediatek,mt6757-camsys.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause) */
+
+#ifndef _DT_BINDINGS_CLK_MT6757_CAMSYS_H
+#define _DT_BINDINGS_CLK_MT6757_CAMSYS_H
+
+#define CLK_CAM_SMI_LARB2	0
+#define CLK_CAM_CAM		1
+#define CLK_CAM_CAMTG		2
+#define CLK_CAM_SENINF		3
+#define CLK_CAM_CAMSV0		4
+#define CLK_CAM_CAMSV1		5
+#define CLK_CAM_CAMSV2		6
+#define CLK_CAM_TSF		7
+
+#endif /* _DT_BINDINGS_CLK_MT6757_CAMSYS_H */
