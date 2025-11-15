b010820fc7dc58ff06b993d1b5174701ce478fa8
bengris32 <bengris32@protonmail.ch>
2025-11-15 00:26:21 +0000

    drm/mediatek: Add support for MT6757 Display OVL-2L
    Add support for the Display OVL-2L hardware on the MT6757 SoC.
    We cannot re-use the MT8183 compatible data since the OVL-2L hardware on
    MT6757 has 8 GMC bits.
    
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- drivers/gpu/drm/mediatek/mtk_disp_ovl.c
+++ drivers/gpu/drm/mediatek/mtk_disp_ovl.c
@@ -671,6 +671,15 @@ static const struct mtk_disp_ovl_data mt2701_ovl_driver_data = {
 	.num_formats = ARRAY_SIZE(mt8173_formats),
 };
 
+static const struct mtk_disp_ovl_data mt6757_ovl_2l_driver_data = {
+	.addr = DISP_REG_OVL_ADDR_MT8173,
+	.gmc_bits = 8,
+	.layer_nr = 2,
+	.fmt_rgb565_is_0 = true,
+	.formats = mt8173_formats,
+	.num_formats = ARRAY_SIZE(mt8173_formats),
+};
+
 static const struct mtk_disp_ovl_data mt8173_ovl_driver_data = {
 	.addr = DISP_REG_OVL_ADDR_MT8173,
 	.gmc_bits = 8,
@@ -742,6 +751,8 @@ static const struct mtk_disp_ovl_data mt8195_ovl_driver_data = {
 static const struct of_device_id mtk_disp_ovl_driver_dt_match[] = {
 	{ .compatible = "mediatek,mt2701-disp-ovl",
 	  .data = &mt2701_ovl_driver_data},
+	{ .compatible = "mediatek,mt6757-disp-ovl-2l",
+	  .data = &mt6757_ovl_2l_driver_data},
 	{ .compatible = "mediatek,mt8173-disp-ovl",
 	  .data = &mt8173_ovl_driver_data},
 	{ .compatible = "mediatek,mt8183-disp-ovl",
