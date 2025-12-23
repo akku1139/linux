f497c90889d90ec85cb61b6c38b63c759beb5ace
bengris32 <bengris32@protonmail.ch>
2025-12-23 16:29:30 +0000

    misc: mediatek: Add skeleton for connectivity drivers
    Add a skeleton directory, Makefile and Kconfig file for preparing
    to build the MediaTek connectivity modules.
    
    Also include a stripped down version of connectivity_build_in_adapter.c
    which is needed for connsys reserved memory setup.
    
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

---
+++ drivers/misc/mediatek/connectivity_build_in_adapter.c
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2016 MediaTek Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
+ */
+
+/*device tree mode*/
+#ifdef CONFIG_OF
+#include <linux/of.h>
+#include <linux/of_irq.h>
+#include <linux/irqreturn.h>
+#include <linux/of_address.h>
+#endif
+
+#include <linux/platform_device.h>
+#include <linux/pm_runtime.h>
+#include <linux/of_reserved_mem.h>
+
+phys_addr_t gConEmiPhyBase;
+EXPORT_SYMBOL(gConEmiPhyBase);
+
+/*Reserved memory by device tree!*/
+
+static int reserve_memory_consys_fn(struct reserved_mem *rmem)
+{
+	pr_info("[W]%s: name: %s,base: 0x%llx,size: 0x%llx\n",
+		__func__, rmem->name, (unsigned long long)rmem->base,
+		(unsigned long long)rmem->size);
+	gConEmiPhyBase = rmem->base;
+	return 0;
+}
+
+RESERVEDMEM_OF_DECLARE(reserve_memory_test, "mediatek,consys-reserve-memory",
+			reserve_memory_consys_fn);
